c1e33b32870cb8364be7e223c5488e94db8af6c0
Bhavadharani-nataraj <53569491+Bhavadharani-nataraj@users.noreply.github.com>
2019-08-15 22:14:29 +0530

    Update delete_end.py

--- delete_end.py
+++ delete_end.py
@@ -2,7 +2,6 @@ class Node:
   def __init__(self,data):
     self.data=data
     self.nextt=None
-
 class SLL:
   def __init__(self):
     self.head=None
@@ -10,11 +9,19 @@ class SLL:
         if self.head is None:
             print("The list has no element to delete")
             return
-
-        n = self.head
-        while n.nextt.nextt is not None:
-            n = n.nextt
-        n.nextt = None
+        temp = self.head
+        while temp.nextt.nextt is not None:
+            temp = temp.nextt
+        temp.nextt = None
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        temp= self.head
+        while temp.nextt is not None:
+            temp= temp.nextt
+        temp.nextt = new_node;
   def printList(self):
       temp=self.head
       while temp!=None:
@@ -24,14 +31,15 @@ class SLL:
 obj=SLL()
 ch=0
 while ch!=4:
-    print("Linked list implementation\n","1.Insertion at begining 2. Deletion 3. Print Llist 4. Exit")
+    print("Linked list implementation\n","1.Insertion at begining 2. Deletion 3. Print list 4. Exit")
     ch=int(input())
     if ch==1:
         print("enter value of the node")
         data=input()
-        index=input()
-        obj.insert_at_index(index,data)
+        obj.insert_at_end(data)
         obj.printList()
     elif ch==2:
         obj.delete_at_end()
         obj.printList()
+    elif ch==3:
+         obj.printList()
