7e1f435237824c9e35275713e28c947d1017b91a
Bhavadharani-nataraj <53569491+Bhavadharani-nataraj@users.noreply.github.com>
2019-08-15 22:02:56 +0530

    Create delete_end.py

---
+++ delete_end.py
@@ -0,0 +1,37 @@
+class Node:
+  def __init__(self,data):
+    self.data=data
+    self.nextt=None
+
+class SLL:
+  def __init__(self):
+    self.head=None
+  def delete_at_end(self):
+        if self.head is None:
+            print("The list has no element to delete")
+            return
+
+        n = self.head
+        while n.nextt.nextt is not None:
+            n = n.nextt
+        n.nextt = None
+  def printList(self):
+      temp=self.head
+      while temp!=None:
+          print(temp,"==>",end='')
+          temp=temp.nextt
+      print("None")  
+obj=SLL()
+ch=0
+while ch!=4:
+    print("Linked list implementation\n","1.Insertion at begining 2. Deletion 3. Print Llist 4. Exit")
+    ch=int(input())
+    if ch==1:
+        print("enter value of the node")
+        data=input()
+        index=input()
+        obj.insert_at_index(index,data)
+        obj.printList()
+    elif ch==2:
+        obj.delete_at_end()
+        obj.printList()
