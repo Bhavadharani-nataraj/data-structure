66eb10d65c38c89b85b6f6f8b6258e2fdfb06106
Bhavadharani-nataraj <53569491+Bhavadharani-nataraj@users.noreply.github.com>
2019-08-15 23:53:23 -0900

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,13 @@
+from pythonds.basic import Queue
+def hotline(namelist,num):
+    simqueue=Queue()
+    for name in namelist:
+        simqueue.enqueue(name)
+    while simqueue.size()>1:
+        for i in range(num):
+            simqueue.enqueue(simqueue.dequeue())
+        simqueue.dequeue()
+        
+    return simqueue.dequeue()
+print(hotline(["babu","devi","subha","kane","able","jeffrey"],7))
+
